b12e039f205b13ab55dd3b4f10ab94930efed4e9
Krista Calderon <kamsci@gmail.com>
2017-05-31 12:47:57 -0700

    Completed factorial tests and code using recursion

--- src/__tests__/factorial_test.ts
+++ src/__tests__/factorial_test.ts
@@ -9,4 +9,34 @@ describe('factorial function', () => {
 
 		expect(result).to.equal(1);
 	});
+
+	it('should return 1 if number is 1', () => {
+		const result = factorial(1);
+
+		expect(result).to.equal(1);
+	});
+
+	it('should return 2 if number is 2', () => {
+		const result = factorial(2);
+
+		expect(result).to.equal(2);
+	});
+
+	it('should return 6 if number is 3', () => {
+		const result = factorial(3);
+
+		expect(result).to.equal(6);
+	});
+
+	it('should return 120 if number is 5', () => {
+		const result = factorial(5);
+
+		expect(result).to.equal(120);
+	});
+
+	it('should return 3,628,800 if number is 10', () => {
+		const result = factorial(10);
+
+		expect(result).to.equal(3628800);
+	});
 });
--- src/factorial.ts
+++ src/factorial.ts
@@ -1,6 +1,8 @@
 export function factorial(num: number): number {
 	if (num === 0) { return 1; }
-	return num;
+	if (num < 0) { return; }
+
+	return num * factorial(num - 1);
 }
 
 export default factorial;
