7f7c1fc1ceef0af222dd899e02820527c1674ef3
Krista Calderon <kamsci@gmail.com>
2017-05-31 11:30:32 -0700

    Added /dist folder to .gitignore; added more tests for reverseOrRotate

--- src/__tests__/reverseOrRotate_test.ts
+++ src/__tests__/reverseOrRotate_test.ts
@@ -4,9 +4,49 @@ import 'mocha';
 
 
 describe('reverseOrRotate function', () => {
-    it('should return an empty string when the size is 0', () => {
-        const result = reverseOrRotate('2471', 0);
+	it('should return an empty string when the size is 0', () => {
+		const result = reverseOrRotate('2471', 0);
 
-        expect(result).to.equal('');
-    });
+		expect(result).to.equal('');
+	});
+
+	it('should return an empty string when the size is greater than the string length', () => {
+		const result = reverseOrRotate('2471', 7);
+
+		expect(result).to.equal('');
+	});
+
+	it('should return an empty string when the string input is empty', () => {
+		const result = reverseOrRotate('', 3);
+
+		expect(result).to.equal('');
+	});
+
+	it('should return all sections reversed if all cubed sums are divisible by 2', () => {
+		const result = reverseOrRotate('247153', 2);
+
+		expect(result).to.equal('421735');
+	});
+
+	it('should return all sections rotated to the left by one position if all cubed sums are NOT divisible by 2', () => {
+		const result = reverseOrRotate('272715333', 3);
+
+		expect(result).to.equal('722157333');
+	});
+
+	it('should return the input string if size is 1', () => {
+		const resultReverse = reverseOrRotate('247153', 1);
+		const resultRotate = reverseOrRotate('272715333', 1)
+
+		expect(resultReverse).to.equal('247153');
+		expect(resultRotate).to.equal('272715333');
+	});
+
+	it('should not return any extra digits that do not complete a section of the given size', () => {
+		const resultReverse = reverseOrRotate('2471536', 2);
+		const resultRotate = reverseOrRotate('27271533311', 3)
+
+		expect(resultReverse).to.equal('421735');
+		expect(resultRotate).to.equal('722157333');
+	});
 });
--- src/reverseOrRotate.ts
+++ src/reverseOrRotate.ts
@@ -1,35 +1,34 @@
 export function reverseOrRotate(str, sz) {
+    /* The input is a string of digits. 
+    Cut the string into chunks of size sz (ignore the last chunk if its size is less than sz).
+    If a chunk represents an integer where the sum of the cubes of its digits is divisible by 2, reverse that chunk; 
+    Otherwise rotate it to the left by one position. 
+    Put together these modified chunks and return the result as a string.
+    */
+
     if (str === "" || sz === 0 || sz > str.length) { return ""; }
     let pointer = sz;
     let sumOfCubes = 0;
     let answer = "";
 
     for (let i = 0; i < str.length; i++) {
-        console.log("i", i, "str[i]", str[i], "pointer", pointer);
-
         if (i < pointer) {
             sumOfCubes += str[i] * str[i];
-            console.log("summing", sumOfCubes);
         }
         if (i === pointer - 1) {
             if (sumOfCubes % 2 === 0) {
                 for (let k = pointer - 1; k > i - sz; k--) {
-                    console.log("k", k, "i - sz", i - sz)
                     answer += str[k];
                 }
             }
             else {
                 let tempStr = str.substring(i - (sz - 2), i + 1);
                 answer += tempStr;
-                console.log("temp", tempStr, "[i - (sz - 1)]", i - (sz - 1))
                 answer += str[i - (sz - 1)];
             }
             pointer += sz;
             sumOfCubes = 0;
         }
-
-        console.log("Answer", answer);
-        console.log("_______________");
     }
     return answer;
 }
